b739a855369cea5ccfceb72a79f692e7118e014e
Zanders <Jarrett.Zanders@gmail.com>
2024-04-13 23:09:27 -0400

    Update sitting.

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -5,7 +5,7 @@
         <ion-content>
           <ion-list id="inbox-list">
             <ion-list-header>PerSpection</ion-list-header>
-            <ion-note>perspection.onrender.com</ion-note>
+            <ion-note>https://perspectionapp.onrender.com/folder/Inbox</ion-note>
 
             <ion-menu-toggle :auto-hide="false" v-for="(p, i) in appPages" :key="i">
               <ion-item @click="selectedIndex = i" router-direction="root" :router-link="p.url" lines="none" :detail="false" class="hydrated" :class="{ selected: selectedIndex === i }">
