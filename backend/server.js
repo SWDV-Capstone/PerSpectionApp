7f82a19498913df43e67bfb433a59f01ba608081
Zanders <Jarrett.Zanders@gmail.com>
2024-04-22 11:32:37 -0400

    Update env and render

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,42 @@
 const express = require('express')
+const cors = require("cors");
+
 const testRoutes = require('./src/test/routes')
 
 require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
 const app = express()
-const port = process.env.PORT || 4000
 app.use(express.json())
 
 // Set up the connection to the database
 const sequelize = new Sequelize(process.env.DB_URL, {
-  dialect: 'postgres',
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
+    dialect: 'postgres',
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
     }
-  }
 })
 
 // Test the connection
-sequelize.sync().then(() => {
-  console.log('Database connected')
-})
-.catch((err) => {
-  console.log('Error connecting to database', err)
-})
+sequelize.sync()
+    .then(() => {
+        console.log('Database connected')
+    })
+    .catch((err) => {
+        console.log('Error connecting to database', err)
+    })
 
 // Define the routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
-app.use('/tests', testRoutes)
-
-app.listen(port, () => console.log(`Server is running on port ${port}`))
+// app.use('/tests', testRoutes)
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`)
+})
