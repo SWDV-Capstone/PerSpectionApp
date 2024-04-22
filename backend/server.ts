907d99b4dc74abc4b67930dc9d21a6bc22ad92bb
Zanders <Jarrett.Zanders@gmail.com>
2024-04-22 18:48:25 -0400

    still fixing /tests

--- backend/server.ts
+++ backend/server.ts
@@ -36,7 +36,7 @@ app.get('/', (req, res) => {
     // res.send('Hello World!')
 })
 
-// app.use('/tests', testRoutes)
+app.use('/tests', testRoutes)
 const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
