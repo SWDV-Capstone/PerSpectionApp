3cfe0755d6225a182e7f801942c6786281949acb
Zanders <Jarrett.Zanders@gmail.com>
2024-04-26 16:32:30 -0400

    CORS again

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var cors = require("cors");
-var testRoutes = require('./src/test/routes');
+const express = require('express');
+const cors = require("cors");
+
+const testRoutes = require('./src/test/routes');
 require('dotenv').config();
-var Sequelize = require('sequelize').Sequelize;
-var app = express();
+
+// const Sequelize = require('sequelize').Sequelize;
+const app = express();
 // app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
@@ -13,33 +15,39 @@ app.use((req, res, next) => {
 
 
 // Set up the connection to the database
-var sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'postgres',
-    logging: false,
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-});
+// const sequelize = new Sequelize(process.env.DB_URL, {
+//     dialect: 'postgres',
+//     logging: false,
+//     dialectOptions: {
+//         ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//         }
+//     }
+// });
+
+
 // Test the connection
-sequelize.sync()
-    .then(function () {
-    console.log('Database connected');
-})
-    .catch(function (err) {
-    console.log('Error connecting to database', err);
-});
+// sequelize.sync()
+//     .then(() => {
+//     console.log('Database connected');
+// })
+//     .catch((err) => {
+//     console.log('Error connecting to database', err);
+// });
+
+
 // Define the routes
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 // app.use('/tests', testRoutes)
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log("Server is running on port ".concat(port));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
 });
+
+
 // pool.initialize()
 //     .then(() => {
 //         console.log("Data Source initialized!")
