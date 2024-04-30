c88696054962e084e28d1f66cff0867bb69caf9d
Zanders <Jarrett.Zanders@gmail.com>
2024-04-30 13:30:01 -0400

    Error fix

--- backend/src/perspectionDb/queries.js
+++ backend/src/perspectionDb/queries.js
@@ -19,13 +19,13 @@ const checkIfPointParamExists = "SELECT t FROM points p WHERE p.param1 = $1"
 const updatePointRow = "UPDATE points SET param1 = $1, param2 = $2, param3 = $3 WHERE id = $4"
 
 // INSPECTION QUERIES
-const getInspections = "SELECT * FROM inspections"
-const getInspectionById = "SELECT * FROM inspections WHERE id = $1"
-const deleteInspectionRow = "DELETE FROM inspections WHERE id = $1"
-const addInspectionRow = "INSERT INTO inspections (param1, param2, param3) VALUES ($1, $2, $3)"
-const checkIfInspectionParamExists = "SELECT t FROM inspections i WHERE i.param1 = $1"
-// const updateInspectionRow = "UPDATE inspections SET param1 = $1 WHERE id = $2"
-const updateInspectionRow = "UPDATE inspections SET param1 = $1, param2 = $2, param3 = $3 WHERE id = $4"
+// const getInspections = "SELECT * FROM inspections"
+// const getInspectionById = "SELECT * FROM inspections WHERE id = $1"
+// const deleteInspectionRow = "DELETE FROM inspections WHERE id = $1"
+// const addInspectionRow = "INSERT INTO inspections (param1, param2, param3) VALUES ($1, $2, $3)"
+// const checkIfInspectionParamExists = "SELECT t FROM inspections i WHERE i.param1 = $1"
+// // const updateInspectionRow = "UPDATE inspections SET param1 = $1 WHERE id = $2"
+// const updateInspectionRow = "UPDATE inspections SET param1 = $1, param2 = $2, param3 = $3 WHERE id = $4"
 
 module.exports = {
     // TEST QUERIES
@@ -43,10 +43,10 @@ module.exports = {
     checkIfPointParamExists,
     updatePointRow,
     // INSPECTION QUERIES
-    getInspections,
-    getInspectionById,
-    deleteInspectionRow,
-    addInspectionRow,
-    checkIfInspectionParamExists,
-    updateInspectionRow
+    // getInspections,
+    // getInspectionById,
+    // deleteInspectionRow,
+    // addInspectionRow,
+    // checkIfInspectionParamExists,
+    // updateInspectionRow
 }
